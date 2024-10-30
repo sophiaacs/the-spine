99d3b9af08301469abc1e8d8af07bdd03eb72cbc
sophia <slim9724@stu.gusd.net>
2024-10-29 23:55:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    document.getElementById('add-book-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const status = document.getElementById('status').value;
+	const rating = document.getElementById('rating').value;
+	const thoughts = document.getElementById('thoughts').value;
+
+        const newBook = document.createElement('p');
+        newBook.textContent = `title: ${title}, author: ${author}, status: ${status}, rating: ${rating}, thoughts: ${thoughts}`;
+
+        document.getElementById('my-books').appendChild(newBook);
+
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('status').value = 'read';
+	document.getElementById('rating').value = '';
+	document.getElementById('thoughts').value = '';
+    });
+});
+
+function openTab(evt, Tab) {
+  
+  var i, tabcontent, tablinks;
+
+ 
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+
+  document.getElementById(Tab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
